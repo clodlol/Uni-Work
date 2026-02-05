b4b40f4137cb4e38df8af4e5fbd9ee78e242530f
claude-3922 <aminabdulrehman532@gmail.com>
2026-02-05 23:24:27 +0500

    task 2 done

--- Object Oriented Programming/LAB 2/task_2.cpp
+++ Object Oriented Programming/LAB 2/task_2.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 using namespace std;
 
+void populateMatrix(int **mat, const int &m, const int &n)
+{
+    for (int i = 0; i < (m * n); i++)
+        mat[i / n][i % n] = rand();
+}
+
+void displayMatrix(int **mat, const int &m, const int &n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int **transposeMatrix(int **mat, const int &m, const int &n)
+{
+    int **trans = new int *[n];
+    for (int i = 0; i < m; i++)
+        trans[i] = new int[m];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            trans[j][i] = mat[i][j];
+        }
+    }
+
+    return trans;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int m = 3, n = 3;
+
+    int **mat = new int *[m];
+    for (int i = 0; i < n; i++)
+        mat[i] = new int[n];
+
+    populateMatrix(mat, m, n);
+    displayMatrix(mat, m, n);
+
+    int **trans = transposeMatrix(mat, m, n);
+    displayMatrix(trans, n, m);
     return 0;
 }
