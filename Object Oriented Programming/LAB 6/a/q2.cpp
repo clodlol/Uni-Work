85e6e24f21d57bf96276fd8f410a4244c6fb063e
claude-3922 <aminabdulrehman532@gmail.com>
2026-03-18 11:23:54 +0500

    miss ko lab nae bnane aati

--- Object Oriented Programming/LAB 6/a/q2.cpp
+++ Object Oriented Programming/LAB 6/a/q2.cpp
@@ -20,6 +20,8 @@ public:
     {
         this->size = arraySize;
         this->strings = new char *[arraySize];
+        for (int i = 0; i < size; ++i)
+            this->strings[i] = nullptr;
     }
 
     ~StringArray()
--- Object Oriented Programming/LAB 6/a/q3.cpp
+++ Object Oriented Programming/LAB 6/a/q3.cpp
@@ -26,8 +26,20 @@ public:
         stringPtr[len] = '\0';
     }
 
-    StringHolder(StringHolder &obj)
+    StringHolder(StringHolder &obj) // shallow copies
     {
+        this->stringPtr = obj.stringPtr;
+    }
+
+    StringHolder(const StringHolder &obj) // deep copies
+    {
+        int len = stringLen(obj.stringPtr);
+        this->stringPtr = new char[len + 1];
+        for (int i = 0; i < len; ++i)
+        {
+            this->stringPtr[i] = obj.stringPtr[i];
+        }
+        this->stringPtr[len] = '\0';
     }
 
     ~StringHolder()
@@ -52,3 +64,19 @@ public:
         cout << "Current stored string: " << stringPtr << "\n";
     }
 };
+
+int main()
+{
+    StringHolder s1("Hello");
+    const StringHolder s2("World");
+    StringHolder s3 = s1; // s3 has a shallow copy
+    StringHolder s4 = s2; // s4 has a deep copy
+
+    s3.setString("Something else");
+    s3.display();
+    s1.display(); // This will produce an error since s3 just deleted the memory s1 is trying to access
+
+    s4.setString("Something else as well");
+    s2.display(); // s2 still retains its contents
+    return 0;
+}
