dd2eddd6ff08075d7efc41949ca39a573024a4b9
clodlol <aminabdulrehman532@gmail.com>
2026-04-05 18:56:02 +0500

    q1 and 2 of assignment 2 done

--- Object Oriented Programming/Assignment 2/q1.cpp
+++ Object Oriented Programming/Assignment 2/q1.cpp
@@ -1,7 +1,171 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
 const float PI = 3.14159265359;
+
+bool equals(double a, double b, double epsilon = 1e-9);
+double arccos(double x);
+double square_root(double x);
+
+class VectorType
+{
+private:
+    double *m_x{};
+    double *m_y{};
+    double *m_z{};
+
+public:
+    VectorType(double x = 0.00, double y = 0.00, double z = 0.00)
+        : m_x{new double(x)}, m_y{new double(y)}, m_z{new double(z)} {}
+    ~VectorType()
+    {
+        delete m_x;
+        delete m_y;
+        delete m_z;
+    }
+    VectorType(const VectorType &vec)
+    {
+        *this = vec;
+    }
+
+    double dotProduct(const VectorType &vec) const
+    {
+        return (*m_x * (*vec.m_x) + *m_y * (*vec.m_y) + *m_z * (*vec.m_z));
+    }
+    double length() const
+    {
+        return square_root(*m_x * (*m_x) + *m_y * (*m_y) + *m_z * (*m_z));
+    }
+    double angle(const VectorType &vec) const
+    {
+        double denom = length() * vec.length();
+        if (equals(denom, 0))
+        {
+            cout << "Division by zero while calculating angles between null vectors. \n";
+            return -1;
+        }
+
+        return arccos(
+            dotProduct(vec) / (length() * vec.length())); // division by zero hogai agar ya phir domain se bahir hua to
+    }
+
+    VectorType &operator=(const VectorType &vec)
+    {
+        if (m_x)
+            delete m_x;
+        if (m_y)
+            delete m_y;
+        if (m_z)
+            delete m_z;
+
+        m_x = new double(*vec.m_x);
+        m_y = new double(*vec.m_y);
+        m_z = new double(*vec.m_z);
+
+        return *this;
+    }
+    VectorType operator+(const VectorType &vec) const
+    {
+        return VectorType{*m_x + *vec.m_x, *m_y + *vec.m_y, *m_z + *vec.m_z};
+    }
+    VectorType operator-(const VectorType &vec) const
+    {
+        return VectorType{*m_x - *vec.m_x, *m_y - *vec.m_y, *m_z - *vec.m_z};
+    }
+    VectorType &operator++()
+    {
+        ++(*m_x), ++(*m_y), ++(*m_z);
+
+        return *this;
+    }
+    VectorType &operator--()
+    {
+        --(*m_x), --(*m_y), --(*m_z);
+
+        return *this;
+    }
+    VectorType operator++(int)
+    {
+        VectorType tempVec{*m_x, *m_y, *m_z};
+        ++(*this);
+        return tempVec;
+    }
+    VectorType operator--(int)
+    {
+        VectorType tempVec{*m_x, *m_y, *m_z};
+        --(*this);
+        return tempVec;
+    }
+    double operator*(const VectorType &vec) const
+    {
+        return dotProduct(vec);
+    }
+    bool operator==(const VectorType &vec2) const
+    {
+        return (equals(*m_x, *vec2.m_x) &&
+                equals(*m_y, *vec2.m_y) &&
+                equals(*m_z, *vec2.m_z));
+    }
+    bool operator!=(const VectorType &vec2) const { return !(*this == vec2); }
+
+    friend ostream &operator<<(ostream &out, const VectorType &vec);
+    friend istream &operator>>(istream &in, VectorType &vec);
+    friend VectorType operator*(const VectorType &vec, double k);
+    friend VectorType operator*(double k, const VectorType &vec);
+};
+
+ostream &operator<<(ostream &out, const VectorType &vec)
+{
+    return out << *vec.m_x << "i "
+               << (*vec.m_y < 0 ? "- " : "+ ") << (*vec.m_y < 0 ? (-*vec.m_y) : *vec.m_y) << "j "
+               << (*vec.m_z < 0 ? "- " : "+ ") << (*vec.m_z < 0 ? (-*vec.m_z) : *vec.m_z) << "k ";
+}
+
+istream &operator>>(istream &in, VectorType &vec)
+{
+    double x{}, y{}, z{};
+
+    while (true)
+    {
+        in >> x >> y >> z;
+        if (!in)
+        {
+            in.clear();
+            in.ignore(numeric_limits<streamsize>::max(), '\n'); // ignores failing data
+            continue;
+        }
+        in.ignore(numeric_limits<streamsize>::max(), '\n'); // ignores extraneous data
+        break;
+    }
+
+    *vec.m_x = x;
+    *vec.m_y = y;
+    *vec.m_z = z;
+    return in;
+}
+
+VectorType operator*(const VectorType &vec, double k)
+{
+    return VectorType{*vec.m_x * k, *vec.m_y * k, *vec.m_z * k};
+}
+VectorType operator*(double k, const VectorType &vec)
+{
+    return vec * k;
+}
+
+int main()
+{
+    VectorType v1{5.2, 6.1, 7.0};
+    VectorType v2{5.2, 6.1, 7.0};
+    cout << (v1 == v2);
+    return 0;
+}
+
+bool equals(double a, double b, double epsilon)
+{
+    return abs(a - b) < epsilon;
+}
 
 double square_root(double x)
 {
@@ -58,133 +222,4 @@ double arccos(double x)
     }
 
     return PI / 2;
-}
-
-class VectorType
-{
-private:
-    double *m_x{};
-    double *m_y{};
-    double *m_z{};
-
-public:
-    VectorType(double x = 0.00, double y = 0.00, double z = 0.00)
-        : m_x{new double(x)}, m_y{new double(y)}, m_z{new double(z)} {}
-    ~VectorType()
-    {
-        delete m_x;
-        delete m_y;
-        delete m_z;
-    }
-    VectorType(const VectorType &vec)
-    {
-        *this = vec;
-    }
-
-    double dotProduct(const VectorType &vec) const
-    {
-        return (*m_x * (*vec.m_x) + *m_y * (*vec.m_y) + *m_z * (*vec.m_z));
-    }
-    double length() const
-    {
-        return square_root(*m_x * (*m_x) + *m_y * (*m_y) + *m_z * (*m_z));
-    }
-    double angle(const VectorType &vec) const
-    {
-        return arccos(
-            dotProduct(vec) / (length() * vec.length())); // division by zero hogai agar ya phir domain se bahir hua to
-    }
-
-    VectorType &operator=(const VectorType &vec)
-    {
-        if (m_x)
-            delete m_x;
-        if (m_y)
-            delete m_y;
-        if (m_z)
-            delete m_z;
-
-        m_x = new double(*vec.m_x);
-        m_y = new double(*vec.m_y);
-        m_z = new double(*vec.m_z);
-
-        return *this;
-    }
-    VectorType operator+(const VectorType &vec) const
-    {
-        return VectorType{*m_x + *vec.m_x, *m_y + *vec.m_y, *m_z + *vec.m_z};
-    }
-    VectorType operator-(const VectorType &vec) const
-    {
-        return VectorType{*m_x - *vec.m_x, *m_y - *vec.m_y, *m_z - *vec.m_z};
-    }
-    VectorType &operator++()
-    {
-        ++(*m_x), ++(*m_y), ++(*m_z);
-
-        return *this;
-    }
-    VectorType &operator--()
-    {
-        --(*m_x), --(*m_y), --(*m_z);
-
-        return *this;
-    }
-    VectorType operator++(int)
-    {
-        VectorType tempVec{*m_x, *m_y, *m_z};
-        ++(*this);
-        return tempVec;
-    }
-    VectorType operator--(int)
-    {
-        VectorType tempVec{*m_x, *m_y, *m_z};
-        --(*this);
-        return tempVec;
-    }
-    double operator*(const VectorType &vec) const
-    {
-        return dotProduct(vec);
-    }
-
-    friend bool operator==(const VectorType &vec1, const VectorType &vec2);
-    friend bool operator!=(const VectorType &vec1, const VectorType &vec2);
-    friend ostream &operator<<(ostream &out, const VectorType &vec);
-    friend istream &operator>>(istream &in, VectorType &vec);
-    friend VectorType operator*(const VectorType &vec, double k);
-    friend VectorType operator*(double k, const VectorType &vec);
-};
-
-bool operator==(const VectorType &vec1, const VectorType &vec2)
-{
-    return (*vec1.m_x == *vec2.m_x && *vec1.m_y == *vec2.m_y && *vec1.m_z == *vec2.m_z);
-}
-
-bool operator!=(const VectorType &vec1, const VectorType &vec2) { return !(vec1 == vec2); }
-
-ostream &operator<<(ostream &out, const VectorType &vec)
-{
-    return out << *vec.m_x << "i "
-               << (*vec.m_y < 0 ? "- " : "+ ") << (*vec.m_y < 0 ? (-*vec.m_y) : *vec.m_y) << "j "
-               << (*vec.m_z < 0 ? "- " : "+ ") << (*vec.m_z < 0 ? (-*vec.m_z) : *vec.m_z) << "k ";
-}
-
-istream &operator>>(istream &in, VectorType &vec)
-{
-    return in >> *vec.m_x >> *vec.m_y >> *vec.m_z; // TODO: implement input validation
-}
-
-VectorType operator*(const VectorType &vec, double k)
-{
-    return VectorType{*vec.m_x * k, *vec.m_y * k, *vec.m_z * k};
-}
-VectorType operator*(double k, const VectorType &vec)
-{
-    return vec * k;
-}
-
-int main()
-{
-    cout << arccos(0.6908);
-    return 0;
 }
--- Object Oriented Programming/Assignment 2/q2.cpp
+++ Object Oriented Programming/Assignment 2/q2.cpp
@@ -1,6 +1,299 @@
 #include <iostream>
 using namespace std;
 
+const int MAX_INPUT_SIZE = 100;
+
+int calculateLen(char *arr);
+void copyString(char *destination, int destAllocSize, char *source);
+int countDigits(int x);
+char *intToCString(int x); // caller must delete[]
+double posFractionalPart(double n);
+char *doubleToCString(double x, int precision); // caller must delete[], known limitation: truncates if all of the fractional part is 0
+bool equals(double a, double b, double epsilon = 1e-9);
+
+class String
+{
+private:
+    char *m_str{};
+    int m_len{};
+
+public:
+    String(char *str = "")
+    {
+        m_len = calculateLen(str);
+        m_str = new char[m_len + 1];
+        copyString(m_str, m_len + 1, str);
+    }
+    ~String() { delete[] m_str; }
+    String(const String &obj) { *this = obj; }
+    String(String &&obj) : m_str{obj.m_str}, m_len{obj.m_len}
+    {
+        obj.m_str = nullptr;
+        obj.m_len = 0;
+    }
+
+    int length() const { return m_len; }
+
+    String &upper()
+    {
+        for (int i = 0; i < m_len; ++i)
+        {
+            if (m_str[i] >= 'a' && m_str[i] <= 'z')
+                m_str[i] += ('a' - 'A');
+        }
+
+        return *this;
+    }
+
+    String &lower()
+    {
+        for (int i = 0; i < m_len; ++i)
+        {
+            if (m_str[i] >= 'A' && m_str[i] <= 'Z')
+                m_str[i] -= ('a' - 'A');
+        }
+
+        return *this;
+    }
+
+    char at(int i) const
+    {
+        if (i < 0 || i >= m_len)
+        {
+            cout << "Bounds error while accessing string element.\n";
+            return 0;
+        }
+
+        return m_str[i];
+    }
+
+    int findIndex(char *substr) const
+    {
+        int subLen = calculateLen(substr);
+        for (int i = 0; i <= m_len - subLen; ++i)
+        {
+            bool found = true;
+            for (int j = i; j < i + subLen; ++j)
+            {
+                if (m_str[j] != substr[j - i])
+                {
+                    found = false;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    int compare(const String &obj) const
+    {
+        int range = min(m_len, calculateLen(obj.m_str));
+        for (int i = 0; i < range; ++i)
+        {
+            if (m_str[i] > obj.m_str[i])
+                return 1;
+            else if (m_str[i] < obj.m_str[i])
+                return -1;
+        }
+
+        return 0;
+    }
+
+    String substring(int start, int end) const
+    {
+        if ((start < 0 || start >= m_len) || (end < 0 || end >= m_len))
+        {
+            cout << "Out of bounds error while calculating substring!\n";
+            return String{};
+        }
+
+        if (start > end)
+        {
+            cout << "Starting index can't be greater than ending index!\n";
+            return String{};
+        }
+
+        char *resStr = new char[(end - start + 1) + 1];
+        int resCounter = 0;
+        // indices end and start are inclusive, extra 1 for null terminator
+        for (int i = start; i <= end; ++i)
+        {
+            resStr[resCounter++] = m_str[i];
+        }
+
+        resStr[resCounter] = '\0';
+
+        String res{resStr};
+
+        delete[] resStr;
+        return res;
+    }
+
+    String &append(const char *substr)
+    {
+        int subLen = strlen(substr);
+        char *temp = m_str;
+        int tempLen = m_len;
+
+        m_len += subLen;
+        m_str = new char[m_len + 1];
+
+        for (int i = 0; i < tempLen; ++i)
+            m_str[i] = temp[i];
+        for (int i = tempLen; i < m_len; ++i)
+        {
+            m_str[i] = substr[i - tempLen];
+        }
+        m_str[m_len] = '\0';
+
+        delete[] temp;
+
+        return *this;
+    }
+    String &append(const string &s) { return append(s.c_str()); }
+    String &append(int x)
+    {
+        const char *substr = intToCString(x);
+        append(substr);
+
+        delete[] substr;
+
+        return *this;
+    }
+    String &append(double x)
+    {
+        const char *substr = doubleToCString(x, 10);
+        append(substr);
+
+        delete[] substr;
+
+        return *this;
+    }
+
+    String &prepend(const char *substr)
+    {
+        int subLen = strlen(substr);
+        char *temp = m_str;
+        int tempLen = m_len;
+
+        m_len += subLen;
+        m_str = new char[m_len + 1];
+
+        for (int i = 0; i < subLen; ++i)
+        {
+            m_str[i] = substr[i];
+        }
+        for (int i = subLen; i < m_len; ++i)
+        {
+            m_str[i] = temp[i - subLen];
+        }
+        m_str[m_len] = '\0';
+
+        delete[] temp;
+
+        return *this;
+    }
+    String &prepend(const string &s) { return prepend(s.c_str()); }
+    String &prepend(int x)
+    {
+        const char *substr = intToCString(x);
+        prepend(substr);
+
+        delete[] substr;
+
+        return *this;
+    }
+    String &prepend(double x)
+    {
+        const char *substr = doubleToCString(x, 10);
+        prepend(substr);
+
+        delete[] substr;
+
+        return *this;
+    }
+
+    String &operator=(const String &obj)
+    {
+        if (m_str)
+            delete[] m_str;
+
+        m_len = obj.m_len;
+        m_str = new char[m_len + 1];
+        strcpy(m_str, obj.m_str);
+
+        return *this;
+    }
+
+    char operator[](int i) { return at(i); }
+
+    bool operator>(const String &obj) const { return compare(obj) > 0; }
+    bool operator<(const String &obj) const { return compare(obj) < 0; }
+    bool operator==(const String &obj) const { return compare(obj) == 0; }
+    bool operator!=(const String &obj) const { return compare(obj) != 0; }
+
+    String operator+(const String &obj) const { return (String{m_str}.append(obj.m_str)); }
+    String operator+(const char *substr) const { return (String{m_str}.append(substr)); }
+    String operator+(const string &s) const { return (String{m_str}.append(s)); }
+    String operator+(int i) const { return (String{m_str}.append(i)); }
+    String operator+(double d) const { return (String{m_str}.append(d)); }
+
+    friend String operator+(const char *substr, const String &obj) { return (String{obj.m_str}.prepend(substr)); }
+    friend String operator+(const string &s, const String &obj) { return (String{obj.m_str}.prepend(s)); }
+    friend String operator+(int i, const String &obj) { return (String{obj.m_str}.prepend(i)); }
+    friend String operator+(double d, const String &obj) { return (String{obj.m_str}.prepend(d)); }
+
+    String &operator+=(const String &obj) { return append(obj.m_str); }
+    String &operator+=(const char *substr) { return append(substr); }
+    String &operator+=(const string &s) { return append(s); }
+    String &operator+=(int i) { return append(i); }
+    String &operator+=(double d) { return append(d); }
+
+    friend ostream &operator<<(ostream &out, const String &obj);
+    friend istream &operator>>(istream &in, String &obj);
+};
+
+ostream &operator<<(ostream &out, const String &obj)
+{
+    return out << obj.m_str;
+}
+
+istream &operator>>(istream &in, String &obj)
+{
+    string s;
+
+    while (true)
+    {
+        in >> s;
+        if (!in)
+        {
+            in.clear();
+            in.ignore(numeric_limits<streamsize>::max(), '\n'); // ignores failing data
+            continue;
+        }
+        in.ignore(numeric_limits<streamsize>::max(), '\n'); // ignores extraneous data
+        break;
+    }
+
+    String temp{(char *)s.c_str()};
+    obj = temp;
+
+    return in;
+}
+
+int main()
+{
+    cout << String{"abdcba"}.substring(1, 3);
+
+    return 0;
+}
+
 int calculateLen(char *arr)
 {
     int c = 0;
@@ -15,7 +308,7 @@ void copyString(char *destination, int destAllocSize, char *source)
 {
     if (destAllocSize <= 0)
     {
-        cout << "Not enough allocated memory to copy string. \n"; // return an error here?
+        cout << "Not enough allocated memory to copy string. \n";
         return;
     }
 
@@ -41,15 +334,19 @@ int countDigits(int x)
 
 char *intToCString(int x) // caller must delete[]
 {
-    const int digits = countDigits(x), size = (x < 0 ? digits + 1 : digits);
+    bool neg = x < 0;
+    if (neg)
+        x = -x;
+
+    const int digits = countDigits(x), size = (neg ? digits + 1 : digits);
     char *temp = new char[size + 1];
 
-    for (int i = size - 1, a = (x < 0 ? -x : x); a != 0 && i >= 0; --i, a /= 10)
+    for (int i = size - 1, a = x; a != 0 && i >= 0; --i, a /= 10)
     {
         temp[i] = '0' + (a % 10);
     }
 
-    if (x < 0)
+    if (neg)
         temp[0] = '-';
 
     temp[size] = '\0';
@@ -57,34 +354,50 @@ char *intToCString(int x) // caller must delete[]
     return temp;
 }
 
-char *doubleToCString(double x, int precision) // caller must delete[]
+double posFractionalPart(double n)
 {
-    int fractionalDigits = 0, integralDigits = countDigits(x);
-    int a = ((int)x);
+    if (n < 0)
+        n = -n;
+    return (n - (int)n);
+}
 
-    for (double i = (x - ((int)x)) * 10; (i - (int)i) > 1e-20 && fractionalDigits <= precision; i *= 10, ++fractionalDigits)
+char *doubleToCString(double x, int precision) // caller must delete[], known limitation: truncates if all of the fractional part is 0
+{
+    if (equals(x, 0.00))
     {
-        i = i - ((int)i);
+        char *res = new char[2];
+        res[0] = '0';
+        res[1] = '\0';
+        return res;
     }
 
-    char *res = new char[(fractionalDigits + integralDigits + 1) + (1) + (x < 0 ? 1 : 0)];
+    int fractionalDigits = 0, integralDigits = countDigits(x);
+
+    for (double i = posFractionalPart(x) * 10; !equals(i, 0.00) && fractionalDigits < precision; i *= 10, ++fractionalDigits)
+    {
+        i = posFractionalPart(i);
+    }
+
+    char *res = new char[(fractionalDigits + integralDigits + 1) + (fractionalDigits > 0 ? 1 : 0) + (x < 0 ? 1 : 0)];
+    // integral digits+fractional digits+null terminator+point+minus sign(if negative)
     int resCounter = 0;
-    if (x < 0)
-        res[resCounter++] = '-';
 
     char *integralPart = intToCString(x);
 
-    for (int i = 0; i < integralDigits; ++i)
+    for (int i = 0; i < integralDigits + (x < 0 ? 1 : 0); ++i)
     {
         res[resCounter++] = integralPart[i];
     }
 
-    res[resCounter++] = '.';
+    if (fractionalDigits > 0)
+    {
+        res[resCounter++] = '.';
+    }
 
-    for (double i = (x - ((int)x)) * 10, c = 0; c < fractionalDigits; i *= 10, ++c)
+    for (double i = posFractionalPart(x) * 10, c = 0; c < fractionalDigits; i *= 10, ++c)
     {
         res[resCounter++] = '0' + (int)i;
-        i = i - ((int)i);
+        i = posFractionalPart(i);
     }
 
     res[resCounter] = '\0';
@@ -93,232 +406,7 @@ char *doubleToCString(double x, int precision) // caller must delete[]
     return res;
 }
 
-class String
+bool equals(double a, double b, double epsilon)
 {
-private:
-    char *m_str{};
-    int m_len{};
-
-    char circularAt(int index)
-    {
-        if (m_len == 0)
-            return '\0';
-        index %= m_len;
-
-        if (index < 0)
-            index = m_len + index;
-
-        return m_str[index];
-    }
-
-public:
-    String(char *str)
-    {
-        m_len = calculateLen(str);
-        m_str = new char[m_len + 1];
-        copyString(m_str, m_len + 1, str);
-    }
-    ~String() { delete[] m_str; }
-    String(const String &obj)
-    {
-        *this = obj;
-    }
-
-    int length() const
-    {
-        return m_len;
-    }
-
-    String &upper()
-    {
-        for (int i = 0; i < m_len; ++i)
-        {
-            if (m_str[i] >= 'a' && m_str[i] <= 'z')
-                m_str[i] += ('a' - 'A');
-        }
-
-        return *this;
-    }
-
-    String &lower()
-    {
-        for (int i = 0; i < m_len; ++i)
-        {
-            if (m_str[i] >= 'A' && m_str[i] <= 'Z')
-                m_str[i] -= ('a' - 'A');
-        }
-
-        return *this;
-    }
-
-    char at(int i)
-    {
-        if (i < 0 || i >= m_len)
-        {
-            cout << "Bounds error while accessing string element.\n";
-            return 0;
-        }
-
-        return m_str[i];
-    }
-
-    int *findIndexAll(char *substr) // caller must delete[]
-    {
-        int subLen = calculateLen(substr), totalFound = 0;
-        if (subLen == 0)
-            return nullptr;
-
-        int *indices = new int[1];
-        indices[0] = -1;
-
-        for (int i = 0; i <= m_len - subLen; ++i)
-        {
-            bool found = true;
-            for (int j = i; j < i + subLen; ++j)
-            {
-                if (m_str[j] != substr[j - i])
-                {
-                    found = false;
-                    break;
-                }
-            }
-
-            if (found)
-            {
-                totalFound++;
-
-                int *temp = new int[totalFound + 1];
-                for (int j = 0; j < totalFound - 1; ++j)
-                    temp[j] = indices[j];
-
-                temp[totalFound - 1] = i;
-                temp[totalFound] = -1;
-                delete[] indices;
-
-                indices = temp;
-            }
-        }
-
-        return indices;
-    }
-
-    int findIndex(char *substr)
-    {
-        int subLen = calculateLen(substr);
-        for (int i = 0; i <= m_len - subLen; ++i)
-        {
-            bool found = true;
-            for (int j = i; j < i + subLen; ++j)
-            {
-                if (m_str[j] != substr[j - i])
-                {
-                    found = false;
-                    break;
-                }
-            }
-
-            if (found)
-            {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
-    int compare(const String &obj)
-    {
-        int range = min(m_len, calculateLen(obj.m_str));
-        for (int i = 0; i < range; ++i)
-        {
-            if (m_str[i] > obj.m_str[i])
-                return 1;
-            else if (m_str[i] < obj.m_str[i])
-                return -1;
-        }
-
-        return 0;
-    }
-
-    String &concat(const char *substr)
-    {
-        int subLen = strlen(substr);
-        char *temp = m_str;
-        int tempLen = m_len;
-
-        m_len += subLen;
-        m_str = new char[m_len + 1];
-
-        for (int i = 0; i < tempLen; ++i)
-            m_str[i] = temp[i];
-        for (int i = tempLen; i < m_len; ++i)
-        {
-            m_str[i] = substr[i - tempLen];
-        }
-        m_str[m_len] = '\0';
-
-        delete[] temp;
-
-        return *this;
-    }
-    String &concat(const string &s) { return concat(s.c_str()); }
-    String &concat(int x)
-    {
-        const char *substr = intToCString(x);
-        concat(substr);
-
-        delete[] substr;
-
-        return *this;
-    }
-    String &concat(double x)
-    {
-        const char *substr = doubleToCString(x, 10);
-        concat(substr);
-
-        delete[] substr;
-
-        return *this;
-    }
-
-    String &operator=(const String &obj)
-    {
-        if (m_str)
-            delete[] m_str;
-
-        m_len = obj.m_len;
-        m_str = new char[m_len + 1];
-        strcpy(m_str, obj.m_str);
-    }
-
-    String &operator+=(const char *substr)
-    {
-        return concat(substr);
-    }
-    String &operator+=(const string &s)
-    {
-        return concat(s);
-    }
-    String &operator+=(int i)
-    {
-        return concat(i);
-    }
-    String &operator+=(double d)
-    {
-        return concat(d);
-    }
-
-    friend ostream &operator<<(ostream &out, const String &obj);
-};
-
-ostream &operator<<(ostream &out, const String &obj)
-{
-    return out << obj.m_str;
-}
-
-int main()
-{
-    cout << doubleToCString(-12.0000000001, 10);
-
-    return 0;
+    return abs(a - b) < epsilon;
 }
