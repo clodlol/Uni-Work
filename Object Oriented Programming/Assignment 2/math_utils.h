53f4a217d00678c80d174978ee18176d80ab8242
claude-3922 <aminabdulrehman532@gmail.com>
2026-04-15 22:28:32 +0500

    uploading headerfiles too

---
+++ Object Oriented Programming/Assignment 2/math_utils.h
@@ -0,0 +1,11 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+double arccos(double x);
+double square_root(double x);
+bool equals(double a, double b, double epsilon = 1e-9);
+int countDigits(int x);
+double posFractionalPart(double n);
+bool startsWithK(int n, int k);
+
+#endif
---
+++ Object Oriented Programming/Assignment 2/string_utils.h
@@ -0,0 +1,7 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+void copyString(char *destination, int destAllocSize, char *source);
+int calculateLen(char *arr);
+
+#endif
